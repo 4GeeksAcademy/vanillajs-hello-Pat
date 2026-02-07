8d337cdd0d54eb1838b9313225d95fe62298e2a4
patrcarrasco <pat.r.carrasco@gmail.com>
2026-02-07 18:06:02 +0000

    Generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+function generateCard() {
+const suits = [
+{ symbol: "♠", color: "black" },
+{ symbol: "♥", color: "red" },
+{ symbol: "♦", color: "red" },
+{ symbol: "♣", color: "black" }
+];
+
+const values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+const randomValue = values[Math.floor(Math.random() * values.length)];
+
+document.querySelector(".top-suit").innerHTML = randomSuit.symbol;
+document.querySelector(".bottom-suit").innerHTML = randomSuit.symbol;
+document.querySelector(".number").innerHTML = randomValue;
+
+document.querySelector(".card").style.color = randomSuit.color;
+}
+
+
+
+// Genera una carta al cargar la página
+generateCard();
+
+// Genera una carta cada 10 segundos
+setInterval(generateCard, 10000);
 };
